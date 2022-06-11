cd0a1d214ccad08728341afc8f45a8372b3c0198
Charles <cbells002@gmail.com>
2022-06-10 20:12:32 -0500

    Kotlin Codeacadmy project mathmagic

---
+++ src/main/kotlin/MathMagic.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val userName = "Charles"
+    var magicNum: Int
+
+    magicNum = 5 * 2
+    magicNum += 3
+    magicNum *= 2
+    magicNum += 5
+    magicNum -= 6
+
+
+    println("$userName 's age is $magicNum")
+}
